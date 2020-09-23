d12119df8adce25d96032821ad76db1f6d38576c
EshanDev <theerapong.b@ru.ac.th>
2020-09-23 16:00:39 +0700

    ADD

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthController extends Controller
 {
@@ -14,21 +17,39 @@ class AuthController extends Controller
 
     public function show_register_form()
     {
-        return view('system.auth');
+
+        $student_code = Session::get('student_code');
+        $student_email = Session::get('student_email');
+        return view('system.registration', compact('student_code', 'student_email'));
     }
 
+
+
+
+
+
+
+    // Verify Registration Code and Redirect to Registration Page.
     public function verify_coded(Request $request)
     {
         $code = $request->input('student_code');
         $email = $request->input('student_email');
 
-        $data = array(
-          'code' => $code,
-          'email' => $email
-        );
 
-        return redirect()->route('auth.register', $data);
+        return Redirect::route('auth.register')
+                        ->with('student_code', $code)
+                        ->with('student_email', $email);
     }
+
+
+
+
+
+
+
+
+
+
 
     public function create_registration_code(Request $request)
     {
--- resources/views/partials/verify_coded.blade.php
+++ resources/views/partials/verify_coded.blade.php
@@ -7,13 +7,33 @@
         </div>
 
         <div class="verify-coded">
-               @foreach($data as $dd)
-                   {{$dd->code}}
-            @endforeach
+            <form action="#" class="verify-coded-form">
+                <div class="group-form">
+                    <fieldset class="border rounded custom-fieldset">
+                        <legend class="w-auto legend">ส่วนที่ 1 รหัสยืนยันสิทธิ์</legend>
+                        <div class="grid-container">
+                            <div class="form-group">
+                                <label for="registration_code">รหัสยืนยันสิทธิ์</label>
+                                <input type="text" class="form-control" name="registration_code" id="registration_code"
+                                    value="{{ old('registration_code') }}" placeholder="กรุณากรอกรหัสยีนยีนสิทธิ์">
+                            </div>
+                            <div class="form-group">
+                                <label for="student_code">รหัสนักศึกษา</label>
+                                <input type="text" name="student_code" id="student_code" class="form-control"
+                                    value="{{ $student_code }}" placeholder="ระบุรหัสนักศึกษา">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="student_email">ที่อยู่อีเมล์</label>
+                                <input type="email" name="student_email" id="student_email" class="form-control"
+                                    value="{{ $student_email }}" placeholder="ระบุที่อยู่อีเมล์">
+                            </div>
+                        </div>
+                    </fieldset>
+                </div>
+            </form>
         </div>
 
 
     </div>
 </div>
-
-
